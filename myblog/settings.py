833d06e27639c7c8dfef62f2b8746cec584438e6
Andree Brazeau <andreebrazeau@gmail.com>
2012-06-03 13:41:40 -0700

    Debug True

--- myblog/settings.py
+++ myblog/settings.py
@@ -1,7 +1,7 @@
 # Django settings for myblog project.
 import dj_database_url
  
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -10,18 +10,18 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': '/users/andreebrazeau/sites/Personal-Website/database.db',                      # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
-    }
-else :
+#if DEBUG:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#            'NAME': '/users/andreebrazeau/sites/Personal-Website/database.db',                      # Or path to database file if using sqlite3.
+#            'USER': '',                      # Not used with sqlite3.
+#            'PASSWORD': '',                  # Not used with sqlite3.
+#            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#        }
+#    }
+#else :
     DATABASES = {'default': dj_database_url.config(default='postgres://localhost')} 
 
 # Local time zone for this installation. Choices can be found here:
