8458054992ad80213a345328c60814b42864d989
Andree Brazeau <andreebrazeau@gmail.com>
2012-06-05 20:08:23 -0700

    removed djcelery

--- myblog/settings.py
+++ myblog/settings.py
@@ -95,6 +95,17 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+	"django.contrib.auth.context_processors.auth",
+	"django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media",
+	"django.core.context_processors.static",
+	"django.core.context_processors.tz",
+	"django.contrib.messages.context_processors.messages",
+)
+
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -132,7 +143,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'blog',
     'kombu.transport.django',
-	'djcelery', 
+	#'djcelery', 
 	'gunicorn',
 )
 
