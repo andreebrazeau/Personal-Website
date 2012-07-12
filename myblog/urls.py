4c1bf43601a022ab665d7c5b3c2b7063b769881e
Andree Brazeau <andreebrazeau@gmail.com>
2012-07-11 23:19:21 -0700

    fixed bug

--- myblog/urls.py
+++ myblog/urls.py
@@ -27,3 +27,10 @@ if settings.DEBUG:
             'document_root': settings.MEDIA_ROOT,
         }),
    )
+
+if settings.DEBUG is False:   #if DEBUG is True it will be served automatically
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$',
+        'django.views.static.serve', 
+        {'document_root':settings.STATIC_ROOT}),
+    )   
