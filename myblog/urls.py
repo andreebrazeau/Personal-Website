bb28a7d8cce87422a51efbd4e51d8bd5af0b246e
Andree Brazeau <andreebrazeau@gmail.com>
2012-07-22 22:28:53 -0700

    modified all connection to portfolio. fixed some typo

--- myblog/urls.py
+++ myblog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     url(r'^blog/(?P<blog_id>\d+)/$', 'blog.views.blog'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','blog.views.index', name = 'index'),
-    url(r'^project/$','blog.views.project', name = 'index'),
+    url(r'^portfolio/$','blog.views.portfolio', name = 'index'),
 
 )
 
